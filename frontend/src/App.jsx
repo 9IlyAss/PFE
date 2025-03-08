38808966598cd646e1c9184a5813b4185949b193
Elvi <elviye@users.noreply.github.com>
2025-03-08 17:36:31 +0000

    updated assets + home page part1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import {BrowserRouter, Routes , Route} from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<UserLayout />}>
-      {/* User layout */}
+      <Route index element={<Home />} />
       </Route>
       <Route>
         {/* Admin layout */}
